1eaae3803488c7465175eb4bcc222571a1baed6a
PP2-21B030774 <azat.amenov@gmail.com>
2022-05-08 03:35:55 +0600

    Lab5

--- Lab5/1.py
+++ Lab5/1.py
@@ -2,7 +2,18 @@
 
 import re
 
-pattern = r'[a]{1}\w{0,}'
+"""pattern = r'[a]{1}\w{0,}'
+txt = input()
+
+print(re.findall(pattern, txt))"""
+
+#pattern = r'\W[+]{1}\d[7]{1}\W[(]\d{0,3}\W[)]\d{0,7}'
+pattern = r'\d[7]'
+#pattern = r'\d{0,11}'
 txt = input()
 
 print(re.findall(pattern, txt))
+"""if re.findall(pattern, txt):
+    print("yes")
+else:
+    print("no")"""
